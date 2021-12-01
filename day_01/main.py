3469f0253e3e1d1e179b3569600743c4325691a3
Andreas Bauer <dev@andreasbauer.org>
2021-12-01 22:51:55 +0100

    Refactor day 1 part 1 solution

--- day_01/main.py
+++ day_01/main.py
@@ -2,18 +2,14 @@ input = open('input.txt', 'r').read().split("\n")
 measurements = [int(i) for i in input]
 
 # part 1
-last = int(measurements.pop(0))
-counter_inc = 0
-counter_dec = 0
-for depth in measurements[0:]:
-	if depth > last:
-		counter_inc += 1
-	else:
-		counter_dec += 1
-	last = depth
-	
-print("depth increased: " + str(counter_inc))
-print("depth decreased: " + str(counter_dec))
+incs = []
+for i in range(1, len(measurements)):
+	is_increase = measurements[i] > measurements[i -1]
+	incs.append(is_increase)
+
+print("Part 1")
+print("depth increased: " + str(sum(incs)))
+print("depth decreased: " + str(len(incs) - sum(incs)))
 
 # part 2
 measurements = [int(i) for i in input]
@@ -37,4 +33,5 @@ def measure_with_window(values, window_size):
 	print("depth decreased: " + str(counter_dec))
 
 
+print("Part 2")
 measure_with_window(measurements,3)
