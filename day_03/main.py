c45368b1a3c4bcbb7f850ba39453970e6b121b38
Andreas Bauer <dev@andreasbauer.org>
2021-12-04 10:09:38 +0100

    Refactor day 3 part 1 by extracting logic into own func

--- day_03/main.py
+++ day_03/main.py
@@ -1,17 +1,21 @@
 input = open('input.txt','r').read().split('\n')
 
-line_size = len(input[0])	
-half = len(input) / 2
+def determine_most_common(input_arr):
+	axial_sum = [0] * line_size
+	line_size = len(input[0])	
+	half = len(input_arr) / 2
 
-axial_sum = [0] * line_size
+	for line in input_arr:
+		for i in range(line_size):
+			as_num = line[i]
+			axial_sum[i] += int(line[i])
 
-for line in input:
-	for i in range(line_size):
-		as_num = line[i]
-		axial_sum[i] += int(line[i])
+	gamma_bool_arr = [int(n > half) for n in axial_sum]
+	epsilon_bool_arr = [int(not b) for b in gamma_bool_arr]
+	return gamma_bool_arr, epsilon_bool_arr
 
-gamma_bool_arr = [int(n > half) for n in axial_sum]
-epsilon_bool_arr = [int(not b) for b in gamma_bool_arr]
+
+gamma_bool_arr, epsilon_bool_arr = determine_most_common(input)
 
 gamma = int("".join(str(b) for b in gamma_bool_arr), 2) 
 epsilon = int(''.join([str(b) for b in epsilon_bool_arr]),2)
