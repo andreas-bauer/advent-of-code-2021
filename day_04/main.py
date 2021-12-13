9ff91087b8e66879b7ea3b32212b9192f2d468ed
Andreas Bauer <dev@andreasbauer.org>
2021-12-13 21:12:05 +0100

    Add solution for day 4 part 2

--- day_04/main.py
+++ day_04/main.py
@@ -3,11 +3,9 @@ inputFile = open('input.txt','r')
 
 draws = inputFile.readline().strip().split(',')
 draws = [int(s) for s in draws]
-boards = inputFile.read().split('\n\n')
-boards = [s.strip().replace('\n', ' ').replace('  ', ' ').split(' ') for s in boards]
-boards = np.array(boards).astype(int)
-
-boards_hit =  [x[:] for x in [[False] * 25] * len(boards)]
+boards_input = inputFile.read().split('\n\n')
+boards_input = [s.strip().replace('\n', ' ').replace('  ', ' ').split(' ') for s in boards_input]
+boards_input = np.array(boards_input).astype(int)
 
 def is_bingo(board):
     h1 = board[0] and board[1] and board[2] and board[3] and board[4]
@@ -23,20 +21,34 @@ def is_bingo(board):
     v5 = board[4] and board[9] and board[14] and board[19] and board[24]
     return h1 or h2 or h3 or h4 or h5 or v1 or v2 or v3 or v4 or v5
 
-hit_mask = np.zeros((len(boards),25), dtype=bool)
-found_bingo = False
-for draw in draws:
-    current_mask = boards == draw
+def play_bingo(boards):
+    hit_mask = np.zeros((len(boards),25), dtype=bool)
+    for draw in draws:
+        current_mask = boards == draw
 
-    hit_mask = np.ma.mask_or(hit_mask, current_mask, copy=False)
+        hit_mask = np.ma.mask_or(hit_mask, current_mask, copy=False)
+        hl = np.ma.size(hit_mask)
+        if np.ma.size(hit_mask) <= 1:
+            continue
+        for i in range(len(hit_mask)):
+            found_bingo = is_bingo(hit_mask[i])
+            if found_bingo:
+                board_not_hits = np.ma.masked_array(boards[i], mask = hit_mask[i])
+                not_hits_sum = board_not_hits.sum()
+                print(board_not_hits)
+                print(str(not_hits_sum) + ' * ' + str(draw) + ' = ' + str(not_hits_sum * draw))
+                return i
+    return -1
 
-    for i in range(len(hit_mask)):
-        found_bingo = is_bingo(hit_mask[i])
-        if found_bingo:
-            board_not_hits = np.ma.masked_array(boards[i], mask = hit_mask[i])
-            not_hits_sum = board_not_hits.sum()
-            print(board_not_hits)
-            print(str(not_hits_sum) + ' * ' + str(draw) + ' = ' + str(not_hits_sum * draw))
-            break
-    if found_bingo:
+play_bingo(boards_input)
+
+# Part 2
+remaining_boards = boards_input
+while True:
+    if len(remaining_boards) == 0:
         break
+    print('---')
+    idx = play_bingo(remaining_boards)
+    if idx == -1:
+        break
+    remaining_boards = np.delete(remaining_boards,(idx), axis=0)
