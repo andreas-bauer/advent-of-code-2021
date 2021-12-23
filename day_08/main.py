7e62b4b5103999dba7f5b89c33f7a0473b9931d0
Andreas Bauer <dev@andreasbauer.org>
2021-12-23 20:28:25 +0100

    Add solution for day 8 part 1

---
+++ day_08/main.py
@@ -0,0 +1,20 @@
+input = open('input.txt', 'r').read().split('\n')
+output_lines = [s.split(' | ')[1] for s in input]
+
+ones = 0
+fours = 0
+sevens = 0
+eights = 0
+for line in output_lines:
+	line_arr = line.split(' ')
+	for element in line_arr:
+		if len(element) == 2:
+			ones += 1
+		elif (len(element)) == 4:
+			fours += 1
+		elif (len(element)) == 3:
+			sevens += 1
+		elif (len(element)) == 7:
+			eights += 1
+
+print('Total: ' + str(ones + fours + sevens + eights))
