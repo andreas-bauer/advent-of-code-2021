fa4750520c6060c0444bc0babbb04e8403cfbbd8
Andreas Bauer <dev@andreasbauer.org>
2022-01-08 15:53:42 +0100

    Lint day_11/main.py with autopep8

--- day_11/main.py
+++ day_11/main.py
@@ -1,45 +1,50 @@
 from collections import namedtuple
 import numpy as np
 
-input = open('input.txt','r').read().split('\n')
+input = open('input.txt', 'r').read().split('\n')
 input = [[int(c) for c in lines] for lines in input]
 x_max = len(input[0])
-y_max = len(input) 
+y_max = len(input)
 
 energies = np.array(input)
-will_flash = lambda arr : len(np.where(arr >= 9)[0]) > 0
+def will_flash(arr): return len(np.where(arr >= 9)[0]) > 0
+
 
 Point = namedtuple('Point', ['x', 'y'])
-Range = namedtuple('Range', ['x_from','x_to', 'y_from','y_to'])
-def first_flash(arr):
-	all = np.where(arr >= 9)
-	return Point(x=all[1][0], y=all[0][0])	
+Range = namedtuple('Range', ['x_from', 'x_to', 'y_from', 'y_to'])
 
-def flash_range(x,y):
-	return Range(
-		x_from= x - (x > 0),
-		x_to= x + (x < x_max),
-		y_from= y - (y > 0),
-		y_to= y + (y < y_max)
-	)
+
+def first_flash(arr):
+    all = np.where(arr >= 9)
+    return Point(x=all[1][0], y=all[0][0])
+
+
+def flash_range(x, y):
+    return Range(
+        x_from=x - (x > 0),
+        x_to=x + (x < x_max),
+        y_from=y - (y > 0),
+        y_to=y + (y < y_max)
+    )
+
 
 total_flashes = 0
-for i in range(1,401):
-	step_flashes = 0
-	while will_flash(energies):
-		first = first_flash(energies)
-		r = flash_range(first.x, first.y)
-		energies[first.y][first.x] = -9_000_000
-		energies[r.y_from:r.y_to + 1,r.x_from:r.x_to + 1] += 1
-		step_flashes += 1
-	print(f'[{i}] Flashed {step_flashes} times')
-	energies += 1
-	energies[energies < 0] = 0
-	total_flashes += step_flashes
-	all_are_flashing = not energies.any()
-	
-	if all_are_flashing:
-		print(f'All are flashing at step {i}')
-		break
+for i in range(1, 401):
+    step_flashes = 0
+    while will_flash(energies):
+        first = first_flash(energies)
+        r = flash_range(first.x, first.y)
+        energies[first.y][first.x] = -9_000_000
+        energies[r.y_from:r.y_to + 1, r.x_from:r.x_to + 1] += 1
+        step_flashes += 1
+    print(f'[{i}] Flashed {step_flashes} times')
+    energies += 1
+    energies[energies < 0] = 0
+    total_flashes += step_flashes
+    all_are_flashing = not energies.any()
+
+    if all_are_flashing:
+        print(f'All are flashing at step {i}')
+        break
 
 print(f'Total flashes: {total_flashes}')
